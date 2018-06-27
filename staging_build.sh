4d45f61bf56490d577b9959a2fd01af4ca119074
Dave Chen <totorochina@hotmail.com>
2018-06-27 15:46:21 +0800

    initial commit

---
+++ staging_build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+REPLACE_THIS_WITH_YOUR_S3_BUCKET_NAME="hzchen-private-ore"
+REGION="cn-north-1"
+STACK="sam-apidemo-staging"
+
+pip install -r requirements.txt -t apidemo/build/ && \
+cp apidemo/*.py apidemo/build/ && \
+
+sam validate && \
+sam package \
+	    --template-file template.yaml \
+	        --output-template-file packaged.yaml \
+		    --s3-bucket $REPLACE_THIS_WITH_YOUR_S3_BUCKET_NAME \
+		    --region $REGION
---
+++ staging_build_and_deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+REPLACE_THIS_WITH_YOUR_S3_BUCKET_NAME="hzchen-private-ore"
+REGION="cn-north-1"
+STACK="sam-apidemo-staging"
+
+pip install -r requirements.txt -t apidemo/build/ && \
+cp apidemo/*.py apidemo/build/ && \
+
+sam validate && \
+sam package \
+	    --template-file template.yaml \
+	        --output-template-file packaged.yaml \
+		    --s3-bucket $REPLACE_THIS_WITH_YOUR_S3_BUCKET_NAME \
+		    --region $REGION && \
+
+sam deploy \
+	    --template-file packaged.yaml \
+	        --stack-name $STACK \
+		    --capabilities CAPABILITY_IAM \
+		        --parameter-overrides Env=staging \
+				DeploymentPreference=AllAtOnce \
+			--region $REGION && \
+
+aws cloudformation describe-stacks \
+	    --stack-name $STACK --query 'Stacks[].Outputs' \
+	    --region $REGION
